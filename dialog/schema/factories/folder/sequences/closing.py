67c2e3b8d601c6435774e28fb4d6e3f87501751d
Rob De Feo <robertodefeo@hotmail.com>
2015-12-17 23:15:26 +0000

    Also do closing sequences!!!

--- dialog/schema/factories/folder/sequences/closing.py
+++ dialog/schema/factories/folder/sequences/closing.py
@@ -1,81 +1,76 @@
+from dialog.schema.elements import Grammar, Prompt, Goto, Output, Input, If, Folder
+from dialog.schema.factories.conditions.system import TerminalExchangeConditions
 from dialog.schema.factories.prompts.generic import GenericPrompt
-
-__author__ = 'robdefeo'
 
 
 class ClosingSequences:
     @staticmethod
     def create():
-        return {
-            "@label": "CLOSING SEQUENCES",
-            "input": [
-                {
-                    (0, "grammar"): {
-                        "item": [
-                            "I should be going",
-                            "$ should be going",
-                            "$ should go",
-                            "$ need to go",
-                            "$ got to go",
-                            "$ gotta run",
-                            "$ gotta go",
-                            "$ need to run",
-                            "$ need to leave",
-                            "$ have to leave",
-                            "$ have to go",
-                            "$ gtg"
-                        ]
-                    },
-                    (1, "output"): {
-                        (0, "prompt"): GenericPrompt.ok(),
-                        (1, "goto"): {
-                            "@ref": "output_did_find_what_looking_for"
-                        },
-                        "@id": "output_2449675"
-                    }
-                },
-                {
-                    (0, "grammar"): {
-                        "item": [
-                            "Thanks for your help",
-                            "$ thank you for your help",
-                            "$ thanks for your help"
-                        ]
-                    },
-                    (1, "output"): {
-                        (0, "prompt"): {
-                            "item": "You are welcome.",
-                            "@selectionType": "RANDOM"
-                        },
-                        (1, "goto"): {
-                            "@ref": "output_did_find_what_looking_for"
-                        }
-                    }
-                },
-                {
-                    (0, "grammar"): {
-                        "item": "Goodbye"
-                    },
-                    (1, "if"): [
-                        {
-                            (0, "cond"): {
-                                "@varName": "Terminal_Exchange",
-                                "@operator": "EQUAL_TO_YES"
-                            },
-                            (1, "goto"): {
-                                "@ref": "output_end_of_conversation"
-                            }
-                        },
-                        {
-                            (0, "cond"): {
-                                "@varName": "Terminal_Exchange",
-                                "@operator": "EQUAL_TO_NO"
-                            },
-                            (1, "goto"): {
-                                "@ref": "output_2449675"
-                            }
-                        }
+        return Folder(
+            label="CLOSING SEQUENCES",
+            children=[
+                Input(
+                    children=[
+                        Grammar(
+                            items=[
+                                "I should be going",
+                                "$ should be going",
+                                "$ should go",
+                                "$ need to go",
+                                "$ got to go",
+                                "$ gotta run",
+                                "$ gotta go",
+                                "$ need to run",
+                                "$ need to leave",
+                                "$ have to leave",
+                                "$ have to go",
+                                "$ gtg"
+                            ]
+                        ),
+                        Output(
+                            _id="output_2449675",
+                            children=[
+                                GenericPrompt.ok(),
+                                Goto(ref="output_did_find_what_looking_for")
+                            ]
+                        )
                     ]
-                }
+                ),
+                Input(
+                    children=[
+                        Grammar(
+                            items=[
+                                "Thanks for your help",
+                                "$ thank you for your help",
+                                "$ thanks for your help"
+                            ]
+                        ),
+                        Output(
+                            children=[
+                                Prompt(
+                                    items=["You are welcome."]
+                                ),
+                                Goto(ref="output_did_find_what_looking_for")
+                            ]
+                        )
+                    ]
+                ),
+                Input(
+                    children=[
+                        Grammar(items=["Goodbye"]),
+                        If(
+                            elements=[
+                                TerminalExchangeConditions.is_yes(),
+                                Goto(ref="output_end_of_conversation")
+                            ]
+                        ),
+                        If(
+                            elements=[
+                                TerminalExchangeConditions.is_no(),
+                                Goto(ref="output_2449675")
+                            ]
+                        )
+                    ]
+                )
             ]
-        }
+        )
--- dialog/schema/factories/variables.py
+++ dialog/schema/factories/variables.py
@@ -1,6 +1,5 @@
 __author__ = 'robdefeo'
 
-
 NAME_RESULTS_COUNT = "results_count"
 NAME_PAGE = "Page"
 
@@ -8,5 +7,7 @@ NAME_USER_NAME = "User_Name"
 
 NAME_COLOR_PREFERENCE = "Color_Preference"
 
-NAME_GREETING_COUNT ="Greeting_Count"
+NAME_GREETING_COUNT = "Greeting_Count"
 NAME_SMALL_TALK_COUNT = "Small_Talk_Count"
+
+NAME_TERMINAL_EXCHANGE = "Terminal_Exchange"
